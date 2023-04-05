080e265b31787a9732a8f81902eed6cb434b14e4
Paul Wilson <paul.wilson.r@gmail.com>
2023-04-05 10:53:52 -0500

    second

--- src/App.js
+++ src/App.js
@@ -1,12 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component} from "react";
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+class App extends Component {
+  render() {
+    return <div>Hello World</div>
+  }
 }
 
 export default App;
